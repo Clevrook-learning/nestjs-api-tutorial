195a1a7dff237c41f22934771d1b85627ce4dfa0
Gautham Joshy <gauthamjoshy32@gmail.com>
2026-03-10 12:31:43 +0530

    making prisma module global

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { User, Bookmark } from "generated/prisma";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 export class AuthService {
+    constructor(private prisma: PrismaService){
+
+    }
 
     signUp(){
         return { message: `I have signed up`}
